3db8974eba16e96f0b3f39cfe24edde24828949e
ahmed <ahmedshehab1000@gmail.com>
2019-04-21 16:32:16 +0200

    ad docx and ebup

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var nodePandoc = require('node-pandoc');
 var fs = require('fs');
 var args, callback;
+var downloadExtenstion;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,10 +16,14 @@ router.get('/home', function (req, res, next) {
 router.post('/pandoc', function (req, res, next) {
     var params = req.body
 
-    console.log(params.text)
-    console
+    console.log(params.to)
 
-    args = '-f ' + params.from + ' -t ' + params.to + '';
+    if (params.to === 'epub' || params.to === 'docx') {
+        downloadExtenstion = params.to;
+        args = '-f ' + params.from + ' -t ' + params.to + ' -o ./converted-content/content.' + params.to + '';
+    } else {
+        args = '-f ' + params.from + ' -t ' + params.to + '';
+    }
 
     callback = function (err, result) {
         if (err) {
@@ -30,8 +35,6 @@ router.post('/pandoc', function (req, res, next) {
             if (err) {
                 return console.log(err);
             }
-
-            console.log("The file was saved!");
         });
         return result;
     };
@@ -49,18 +52,16 @@ router.post('/upload', function (req, res, next) {
 
 router.get('/download', function (req, res, next) {
     var path = require('path');
-    res.download(path.join('./converted-content', 'content'), 'content')
-    /*res.download('./converted-content/content', function(err){
-        if(err) {
-            // Check if headers have been sent
-            if(res.headersSent) {
-                // You may want to log something here or do something else
-            } else {
-                return res.sendStatus(err); // 404, maybe 500 depending on err
-            }
-        }
-        // Don't need res.end() here since already sent
-    })*/
+
+    if (downloadExtenstion === 'docx') {
+        res.download(path.join('./converted-content', 'content.docx'), 'content.docx')
+
+    } else if (downloadExtenstion === 'epub') {
+        res.download(path.join('./converted-content', 'content.epub'), 'content.epub')
+    } else {
+        res.download(path.join('./converted-content', 'content'), 'content')
+
+    }
 });
 
 
